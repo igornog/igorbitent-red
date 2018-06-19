7c666aabdef6690efd2ce3f687599147d1b3db45
Igor <igorcmnog@gmail.com>
2018-06-19 16:44:39 -0700

    journal page - css

--- themes/igorbitent/sidebar.php
+++ themes/igorbitent/sidebar.php
@@ -7,11 +7,11 @@
 
  if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
-}
+	}
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
+	<div id="secondary" class="widget-area" role="complementary">
 	
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 
-</div>
+	</div>
